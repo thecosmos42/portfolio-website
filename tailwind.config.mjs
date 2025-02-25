34eed31ffa6f487f5be5cd3a02e8908790c1cde7
thecosmos42 <46294463+thecosmos42@users.noreply.github.com>
2025-02-25 23:33:20 +0400

    modified next.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/assets/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#3c3c3cx`',
       },
+      fontFamily: {
+        'Outfit': ['Outfit', 'sans-serif'],
+        'Ovo': ['Ovo', 'serif']
+      },
+      boxShadow: {
+        'black': '4px 4px 0 #000',
+        'white': '4px 4px 0 #fff',
+      }
     },
   },
   plugins: [],
